d28fcbe5713186a6628be7a452277b5001af76a6
SrashtiGarg <gsrashti09@gmail.com>
2024-06-27 16:26:22 +0530

    initailcommit_newapp

---
+++ NewDemoApp/NewDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDemoAppApp.swift
+//  NewDemoApp
+//
+//  Created by Srashti Garg on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NewDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
